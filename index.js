72ab2590f150b168d743ebdd583214fda6183900
nahidmursaline <nahid15-12408@diu.edu.bd>
2023-12-13 03:06:44 +0600

    details api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,19 @@ async function run() {
       const newBlog = req.body;
       console.log(newBlog)
       const result = await blogCollection.insertOne(newBlog);
+      res.send(result);
+    })
+
+    app.get('/blog', async(req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/blog/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await blogCollection.findOne(query)
       res.send(result);
     })
 
